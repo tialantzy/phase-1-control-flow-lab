7ed52df4b398043c1ac5ddf3465f2026ad382cee
tialantzy <tialantzy@gmail.com>
2022-06-29 17:53:54 -0500

    commit tree a621cbf7cb17b4a5387e665ff78b95d61086b403

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(price){ 
+  if (price <= 400) {
+    return 'This one is on me!'; 
+  } else if (400 > price || price < 1900) {
+    return 'That will be twenty bucks.';
+  } else if (2000 > price || price < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (price >= 2500) {
+    return 'No can do.'; 
+  } 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.': 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous' :
+      return "Thank you so much."; 
+    break; 
+    case 'not as generous' :
+      return "Thank you."; 
+    break; 
+    default: 
+      return "Bye."  
+  }
 }
